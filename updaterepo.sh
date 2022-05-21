28b6025034906086ccfca74ec714960289fce282
xcodeon1 <xcodextest@gmail.com>
2022-05-21 01:37:18 -0400

    sss

--- updaterepo.sh
+++ updaterepo.sh
@@ -1,9 +1,28 @@
-rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release
+#!/bin/bash
+script_full_path=$(dirname "$0")
+cd $script_full_path || exit 1
 
-	apt-ftparchive packages ./debians > Packages
-	gzip -c9 Packages > Packages.gz
-	xz -c9 Packages > Packages.xz
-	zstd -c19 Packages > Packages.zst
-	bzip2 -c9 Packages > Packages.bz2
-	
-echo "Done!"
+rm Packages Packages.bz2 Packages.xz Packages.zst Release Release.gpg
+
+echo "[Repository] Generating Packages..."
+apt-ftparchive packages ./pool > Packages
+zstd -q -c19 Packages > Packages.zst
+xz -c9 Packages > Packages.xz
+bzip2 -c9 Packages > Packages.bz2
+
+echo "[Repository] Generating Release..."
+apt-ftparchive \
+		-o APT::FTPArchive::Release::Origin="Sileo" \
+		-o APT::FTPArchive::Release::Label="Sileo" \
+		-o APT::FTPArchive::Release::Suite="stable" \
+		-o APT::FTPArchive::Release::Version="2.0" \
+		-o APT::FTPArchive::Release::Codename="ios" \
+		-o APT::FTPArchive::Release::Architectures="iphoneos-arm" \
+		-o APT::FTPArchive::Release::Components="main" \
+		-o APT::FTPArchive::Release::Description="Sileo for Checkra1n and Unc0ver" \
+		release . > Release
+
+echo "[Repository] Signing Release using Amy's GPG Key..."
+gpg -abs -u 816C7A50B575162DC29288CD72339224580758CE -o Release.gpg Release
+
+echo "[Repository] Finished"
