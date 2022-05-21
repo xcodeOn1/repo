0ede7aa35484c2e37642f7812b76b745837f0419
xcodeOn1 <65957444+xcodeOn1@users.noreply.github.com>
2022-05-20 22:30:47 -0700

    Update updaterepo.sh

--- updaterepo.sh
+++ updaterepo.sh
@@ -5,7 +5,5 @@ rm Packages Packages.xz Packages.gz Packages.bz2 Packages.zst Release
 	xz -c9 Packages > Packages.xz
 	zstd -c19 Packages > Packages.zst
 	bzip2 -c9 Packages > Packages.bz2
-
-	apt-ftparchive release -c ./assets/repo/repo.conf . > Release
-
+	
 echo "Done!"
